9c1bb3366d13a6e6f8cfb2c96f323b2836005fe1
okosun <okosunmicheal123@gmail.com>
2023-12-12 23:08:48 -0700

    update js

--- Count.js
+++ Count.js
@@ -1,44 +1,58 @@
-const year = document.getElementbyid("year");
-const time = document.getElementbyid("time");
-const day = document.getElementbyid("day");
-const mintues = document.getElementbyid("mintues");
-const secounds = document.getElementbyid("secounds");
-const loading = document.getElementById("loading");
+ const year = document.getElementbyid("year");
+ const down = document.getElementbyid("down");
+ const days = document.getElementbyid("days");
+const minutes = document.getElementbyid("minutes");
+const seconds = document.getElementbyid("seconds");
+ const loading = document.getElementById("loading");
+const countdown = document.getElementById("countdown");
+
 
 const NewYearCountdown = new Date().getFullYear();
-const newdate = new Date(` january 01     ${ NewYearCountdown  + 1}00: 00: 00`);
+const NewYEar = new Date(` january 01    ${NewYearCountdown  + 1} 00: 00: 00`);
+
+// my h1 styling
+
+const countdownElement = document.getElementById('small');
+
+
+countdownElement.style.color = 'blue';
+countdownElement.style.fontWeight = 'bold';
+countdownElement.style.fontSize = '48px';
 
 
 
-year.innerHTML = NewYearCountdown + 1;
+
+
+ year.innerHTML = NewYearCountdown + 1;
 
 
 
 function updateCountdown(){   
 
-    NewYearCountdown =new Date();
-    const diff = NewYearCountdown - newdate;
+    NewYearCountdown =  new Date();
+    
+    
+    const diff = NewYearCountdown - NewYEar;
 
     const d = Math.floor(diff / 1000 / 60 / 60 / 24);
     const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-    const m = Math.floor(diff / 1000 / 60) % 60;
+   const m = Math.floor(diff / 1000 / 60) % 60;
     const s = Math.floor(diff / 1000) % 60;
 
     
-    day.innerHTML = d;
+    days.innerHTML = d;
     hours.innerHTML = h < 10 ? '0' + h : h;
-    mintues.innerHTML = m < 10 ? '0' + m : m;
-    secounds.innerHTML = s < 10 ? '0' + s : s;
+   minutes.innerHTML = m < 10 ? '0' + m : m;
+   seconds.innerHTML = s < 10 ? '0' + s : s;
 }
-
-setTimeout(() => {
+ setTimeout(() => {
     loading.remove();
-    countdown.style.display = 'flex';
-}, 1000);
+     countdown.style.display = 'flex';
+ }, 1000);
 
 
 
 
-setInterval(updateCountdown, 1000);
+// setInterval(updateCountdown, 1000);
 
 
