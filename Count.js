7fcede6a6a1015d2a468c9dc2f2309cd0ee3c3c1
okosun <okosunmicheal123@gmail.com>
2023-12-13 20:16:10 -0700

    update

--- Count.js
+++ Count.js
@@ -1,51 +1,75 @@
- const year = document.getElementbyid("year");
- const down = document.getElementbyid("down");
- const days = document.getElementbyid("days");
-const minutes = document.getElementbyid("minutes");
-const seconds = document.getElementbyid("seconds");
- const loading = document.getElementById("loading");
 const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
+const days = document.getElementById("days");
+const hours = document.getElementsByClassName("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const loading = document.getElementById("loading");
 
 
 const NewYearCountdown = new Date().getFullYear();
-const NewYEar = new Date(` january 01    ${NewYearCountdown  + 1} 00: 00: 00`);
-
-// my h1 styling
-
-const countdownElement = document.getElementById('small');
-
-
-countdownElement.style.color = 'blue';
-countdownElement.style.fontWeight = 'bold';
-countdownElement.style.fontSize = '48px';
+const NewYear = new Date(`January 01 ${NewYearCountdown + 1} 00: 00: 00`);
 
 
 
+// const countdownElement = document.getElementById('small');
 
 
- year.innerHTML = NewYearCountdown + 1;
+//  countdownElement.style.color = 'blue';
+// countdownElement.style.fontWeight = 'bold';
+// countdownElement.style.fontSize = '48px';
 
 
 
-function updateCountdown(){   
 
-    NewYearCountdown =  new Date();
+
+year.innerHTML = NewYearCountdown + 1;
+
+
+
+
+
+
+
+
+
+function updateCountdown() {   
+    
+    const NewYearCountdown = new Date();
+   const diff = NewYearCountdown - NewYear;
     
     
-    const diff = NewYearCountdown - NewYEar;
-
-    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const d = Math.floor(diff / 1000 /60/60 / 24);
     const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-   const m = Math.floor(diff / 1000 / 60) % 60;
-    const s = Math.floor(diff / 1000) % 60;
-
+    const m = Math.floor(diff / 1000 / 60) % 60;
+   const s = Math.floor(diff / 1000) % 60;   
     
-    days.innerHTML = d;
-    hours.innerHTML = h < 10 ? '0' + h : h;
-   minutes.innerHTML = m < 10 ? '0' + m : m;
-   seconds.innerHTML = s < 10 ? '0' + s : s;
-}
- setTimeout(() => {
+
+  days.innerHTML = d;
+minutes.innerHTML = m < 10 ? "0" + m : m;
+    seconds.innerHTML = s < 10 ? "0" + s : s;
+    hours.innerHTML = h < 10 ? "0" + h : h;
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+ 
+
+
+setTimeout(() => {
     loading.remove();
      countdown.style.display = 'flex';
  }, 1000);
@@ -53,6 +77,6 @@ function updateCountdown(){
 
 
 
-// setInterval(updateCountdown, 1000);
+ setInterval(updateCountdown, 1000);
 
 
